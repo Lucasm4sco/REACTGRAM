845ef16ce30e048b0d236d8a5f2033c0026b42b0
Lucasm4sco <lmbp202@gmail.com>
2023-01-24 19:28:57 -0300

    componente menssagem e algumas configurações de autenticação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './hooks/useAuth';
 import './App.css';
 
 import Home from './pages/Home/Home';
@@ -9,15 +10,29 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
+  const { auth, loading } = useAuth();
+
+  if (loading)
+    return <p>Carregando...</p>
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route
+              path='/'
+              element={auth ? <Home /> : <Navigate to='login' />}
+            />
+            <Route
+              path='/login'
+              element={auth ? <Navigate to='/' /> : <Login />}
+            />
+            <Route
+              path='/register'
+              element={auth ? <Navigate to='/' /> : <Register />}
+            />
           </Routes>
         </div>
         <Footer />
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -2,22 +2,75 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Auth.css';
 
+// Redux
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../../slices/authSlice';
+
+import Message from '../../components/Message/Message';
+
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const {loading, error} = useSelector(state => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    dispatch(register(user));
   }
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch])
 
   return (
     <div id='register'>
       <h2>ReactGram</h2>
       <p className='subtitle'>Cadastre-se para compartilhar fotos.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Name' />
-        <input type="email" placeholder='E-mail' />
-        <input type="password" placeholder='Senha' />
-        <input type="password" placeholder='Confirmar senha' />
-        <input type="submit" value='Cadastrar' />
+        <input
+          type="text"
+          placeholder='Name'
+          value={name || ''}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder='E-mail'
+          value={email || ''}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder='Senha'
+          value={password || ''}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder='Confirmar senha'
+          value={confirmPassword || ''}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input 
+          type="submit" 
+          value={loading? 'Aguarde...' : 'Cadastrar'} 
+          disabled={loading}
+        />
+        {error && <Message message={error}  type='error'/>}
       </form>
       <p>
         Já tem conta? <Link to='/login'>Entrar</Link>
